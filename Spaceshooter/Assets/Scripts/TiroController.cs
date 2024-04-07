92679034d4f94fd8cd03c81c00ebc727c7a561ad
Victor Real <victor.realchisto30@hotmail.com>
2024-04-06 23:04:24 -0300

    refactoring shoot movement

--- Spaceshooter/Assets/Scripts/TiroController.cs
+++ Spaceshooter/Assets/Scripts/TiroController.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TiroController : MonoBehaviour
-{
+{   
+    //Velocidade default do tiro
     [SerializeField] private float shootSpeed = 10f;
 
+    //Variável que guarda o Rigidbody do Player
     private Rigidbody2D shootRB;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Acessando Rigidbody
         shootRB = GetComponent<Rigidbody2D>();
+
+        //Fazendo o tiro se mover pra cima
+        //NOTE: Como a velocidade do tiro é constante e ele está sendo movido pelo Rigidbody, ele não precisa ficar no Update.
+        shootRB.velocity = Vector2.up * shootSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        shootRB.velocity = Vector2.up * shootSpeed;
+        
     }
 }
