dd05f52844374afb32ee019dc8a85bc1898d5681
Victor Real <victor.realchisto30@hotmail.com>
2024-04-06 22:00:25 -0300

    created movement system to player

--- Spaceshooter/Assets/Scripts/PlayerController.cs
+++ Spaceshooter/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Instanciando variável que contém o Rigidbody do player
+    private Rigidbody2D rigidbody;
+    //Determinando velocidade do player
+    [SerializeField] private float playerSpeed = 5f;
+
+    //Variáveis que guardarão a velocidade vertical e horizontal do player
+    private float horizontalSpeed;
+    private float verticalSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Coletando o Rigidbody do player
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
 
+    //Movendo o Player
+    private void Move()
+    {
+        //Detectando se o jogador está pressionando os botões de movimento e multiplicando pela velocidade padrão do player
+        //NOTE: Os botões de movimento são os definidos no Input Manager, em "Edit > Project Settings > InputManager"
+        horizontalSpeed = Input.GetAxis("Horizontal") * playerSpeed;
+        verticalSpeed = Input.GetAxis("Vertical") * playerSpeed;
+
+        //adicionando velocidade pela variável velocity do Rigidbody, passando as velocidades calculadas anteriormente como vetor
+        rigidbody.velocity = new Vector2(horizontalSpeed, verticalSpeed);
     }
 }
