956dea386b306e05b8346f5cbe39105f0d0ab97a
Victor Real <victor.realchisto30@hotmail.com>
2024-04-06 22:45:36 -0300

    using rigidbody on shoot object

--- Spaceshooter/Assets/Scripts/TiroController.cs
+++ Spaceshooter/Assets/Scripts/TiroController.cs
@@ -6,15 +6,17 @@ public class TiroController : MonoBehaviour
 {
     [SerializeField] private float shootSpeed = 10f;
 
+    private Rigidbody2D shootRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shootRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.up * Time.deltaTime * shootSpeed;
+        shootRB.velocity = Vector2.up * shootSpeed;
     }
 }
