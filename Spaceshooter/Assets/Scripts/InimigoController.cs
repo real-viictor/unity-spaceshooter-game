af3b5d1ebc19f7a75fbbffa65a208778f442deff
Victor Real <victor.realchisto30@hotmail.com>
2024-04-08 19:48:48 -0300

    commented enemycontroller

--- Spaceshooter/Assets/Scripts/InimigoController.cs
+++ Spaceshooter/Assets/Scripts/InimigoController.cs
@@ -8,7 +8,9 @@ public class InimigoController : MonoBehaviour
 
     private float enemySpeed = 1.5f;
 
-    private float shotTimer = 3f;
+    private float shotTimer;
+
+    private float shotDelay = 3f;
 
     private bool isInPosition = false;
 
@@ -30,21 +32,31 @@ public class InimigoController : MonoBehaviour
         Shoot();
     }
 
+    //Para o inimigo na altura da tela desejada
     private void Stop()
     {
         //Parando o inimigo na posição desejada da tela
         if (transform.position.y <= 3.5f)
         {
             enemyRB.velocity = Vector2.down * 0;
+            //Informando a variável de controle que o inimigo pode começar a atirar, pois já se posicionou
             isInPosition = true;
+
+            //Definindo o tempo do primeiro disparo após se posicionar
+            shotTimer = shotDelay;
         }
     }
 
-    private void Shoot(float shotDelay = 3f)
+    //Atira quando posicionado
+    private void Shoot()
     {
+        //Se o inimigo já chegou no local da tela desejado, então comece a contagem do timer
         if(isInPosition)
         {
+            //Reduzindo o Timer até zerar
             shotTimer -= Time.deltaTime;
+
+            //Ao zerar o Timer, redefine o timer e instancia um tiro
             if(shotTimer <= 0)
             {
                 shotTimer = shotDelay;
