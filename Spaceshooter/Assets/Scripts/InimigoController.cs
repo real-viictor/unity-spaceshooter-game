9462dec209b6765cfb3550b4f635961f74b7016f
Victor Real <victor.realchisto30@hotmail.com>
2024-04-08 07:48:49 -0300

    created enemy movement script

--- Spaceshooter/Assets/Scripts/InimigoController.cs
+++ Spaceshooter/Assets/Scripts/InimigoController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class InimigoController : MonoBehaviour
 {
+    private Rigidbody2D enemyRB;
+
+    private float enemySpeed = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRB = GetComponent<Rigidbody2D>();
+
+        //Definindo movimentação do inimigo para baixo
+        enemyRB.velocity = Vector2.down * enemySpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Stop();
+    }
+
+    private void Stop()
+    {
+        //Parando o inimigo na posição desejada da tela
+        if (transform.position.y <= 3.5f)
+        {
+            enemyRB.velocity = Vector2.down * 0;
+        }
     }
 }
