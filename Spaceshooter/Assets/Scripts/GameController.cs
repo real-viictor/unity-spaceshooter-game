d1b4af585202324f9a7d56a4f82e8567ca55df52
Victor Real <victor.realchisto30@hotmail.com>
2024-04-11 20:46:40 -0300

    commenting gameController script

--- Spaceshooter/Assets/Scripts/GameController.cs
+++ Spaceshooter/Assets/Scripts/GameController.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    //Variável que controla o timer do spawner de inimigos
     private float spawnerTimer;
 
+    //Recebe o objeto do inimigo
     [SerializeField] private GameObject enemyObject;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        //Definindo o timer do primeiro Spawn
         spawnerTimer = 3f;
     }
 
@@ -21,12 +24,15 @@ public class GameController : MonoBehaviour
         SpawnEnemies();
     }
 
+    //Spawnando inimigos baseado em timer
     private void SpawnEnemies()
     {
         spawnerTimer -= Time.deltaTime;
+
+        //Quando o timer zera, redefine o timer em um valor aleatório e instancia o inimigo em algum ponto da tela
          if (spawnerTimer <= 0)
         {
-            spawnerTimer = 2;//Random.Range(0.5f, 2f);
+            spawnerTimer = Random.Range(1f, 3f);
             Instantiate(enemyObject, new Vector3(Random.Range(-8f,8f),8,0), Quaternion.identity);
         }
     }
