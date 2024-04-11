af2086e6b9420f425af86243d01b9e8e36cc9448
Victor Real <victor.realchisto30@hotmail.com>
2024-04-11 14:32:25 -0300

    refactored shot impact fx destroyer

--- Spaceshooter/Assets/Scripts/FXController.cs
+++ Spaceshooter/Assets/Scripts/FXController.cs
@@ -6,14 +6,19 @@ public class FXController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-        //Destruindo o objeto de efeito após o fim da animação, passando o tempo da animação como timer
-        Destroy(gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
+    { 
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void DestroyFX()
+    {
+        //Destruindo o objeto de efeito após o fim da animação, chamando ele como um Animation Event da animação
+        Destroy(gameObject);
     }
 }
