4ba103ec80c7f4499d269b0d8e9a8fa1fa61afdd
Victor Real <victor.realchisto30@hotmail.com>
2025-05-06 22:20:33 -0300

    removed unused variable

--- Spaceshooter/Assets/Scripts/ShotController.cs
+++ Spaceshooter/Assets/Scripts/ShotController.cs
@@ -6,8 +6,6 @@ public class ShotController : MonoBehaviour
 
     [SerializeField] private GameObject shotExplosion;
 
-    [SerializeField] private string origin;
-
     // Start is called before the first frame update
     void Start()
     {
