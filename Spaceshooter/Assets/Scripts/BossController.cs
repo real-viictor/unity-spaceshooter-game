62153d6b6f4dfea4544ab39664d8f6342fccd55a
Victor Real <victor.realchisto30@hotmail.com>
2025-05-07 23:18:58 -0300

    created boss spawn animation

--- Spaceshooter/Assets/Scripts/BossController.cs
+++ Spaceshooter/Assets/Scripts/BossController.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
 
-public class BossController : MonoBehaviour
+public class BossController : Entity
 {
+    private bool isFightStarted;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       canBeHit = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Attack();
+    }
+
+    private void Attack()
+    {
+        if (isFightStarted)
+        {
+            //TODO: State Machine
+            Debug.Log("Attack");
+        }
+    }
+
+    //Utilizado como evento da animação
+    private void StartFight()
+    {
+        canBeHit = true;
+        isFightStarted = true;
     }
 }
--- Spaceshooter/Assets/Scripts/Entity.cs
+++ Spaceshooter/Assets/Scripts/Entity.cs
@@ -8,10 +8,12 @@ public class Entity : MonoBehaviour
     //Variável que guarda o prefab de explosão do inimigo
     [SerializeField] protected GameObject explosion;
 
+    [SerializeField] protected bool canBeHit;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        canBeHit = true;
     }
 
     // Update is called once per frame
@@ -24,7 +26,10 @@ public class Entity : MonoBehaviour
     public virtual void LoseHealth(int damage)
     {
         //Reduzindo a vida baseado no dano
-        entityHealth -= damage;
+        if (canBeHit)
+        {
+            entityHealth -= damage;
+        }
 
         //Se o inimigo zerar a vida, instancie a explosão no local onde ele estava e destrua o inimigo
         if (entityHealth <= 0)
