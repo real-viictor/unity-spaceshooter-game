5feaba0dc3952f3ce3ab11172efa7fb4af12be71
Victor Real <victor.realchisto30@hotmail.com>
2025-05-28 22:59:47 -0300

    created boss defeat animation and controller

--- Spaceshooter/Assets/Scripts/BossDefeatController.cs
+++ Spaceshooter/Assets/Scripts/BossDefeatController.cs
@@ -1,9 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BossDefeatController : MonoBehaviour
 {
+    [SerializeField] private GameObject smallExplosion, bossExplosion;
+    [SerializeField] private Transform[] explosionSpots;
+
+    private float explosionCreationTimer;
+    private int lastExplosionIndex, currentExplosionIndex;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,31 @@ public class BossDefeatController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CreateExplosion();
+    }
+
+    private void CreateExplosion()
+    {
+        if (explosionCreationTimer > 0f)
+        {
+            explosionCreationTimer -= Time.deltaTime;
+        } else
+        {
+            do
+            {
+                currentExplosionIndex = Random.Range(0, explosionSpots.Length);
+            } while (currentExplosionIndex == lastExplosionIndex);
+
+            Instantiate(smallExplosion, explosionSpots[currentExplosionIndex]);
+            lastExplosionIndex = currentExplosionIndex;
+            explosionCreationTimer = 0.3f;
+        }
         
+    }
+
+    private void DestroyBoss()
+    {
+        Destroy(gameObject);
+        Instantiate(bossExplosion, transform.position, Quaternion.identity);
     }
 }
